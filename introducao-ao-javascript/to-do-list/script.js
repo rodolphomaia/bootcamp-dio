dd598798dc51351b82156116e3fe796b8d711241
Rodolpho Maia <rodolpho.java@gmail.com>
2021-12-09 21:32:24 -0300

    Finalizado a to-do list

---
+++ introducao-ao-javascript/to-do-list/script.js
@@ -0,0 +1,46 @@
+var count = 0;
+
+window.addEventListener('load', () => {
+    let addTaskBtn = document.querySelector('.addTask');
+    
+    addTaskBtn.addEventListener('click', () => {
+        addTaskToList();
+    });
+
+});
+
+function addTaskToList(){
+    let taskTextField = document.querySelector('.task');
+    let taskList = document.querySelector('.taskList');
+    let newTask = taskTextField.value
+    
+    let div = document.createElement('div');
+
+    let label = document.createElement('label');
+    label.setAttribute('for', count);
+
+    let checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('id', count);
+
+    if(newTask != '') {
+        label.innerHTML = newTask;
+        div.append(checkbox, label);
+        taskList.append(div);
+    }
+
+    document.addEventListener('click', () => {
+        checkTask(checkbox);
+    });
+
+    count++;
+}
+
+function checkTask(el) {
+    console.log(el)
+    if(el.checked) {
+        el.nextElementSibling.setAttribute('class', 'line-through')
+    } else {
+        el.nextElementSibling.setAttribute('class', '')
+    }
+}
